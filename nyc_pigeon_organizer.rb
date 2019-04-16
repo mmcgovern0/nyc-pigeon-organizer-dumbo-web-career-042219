6d4e9e65953bd96f19b57a102fb13e3bcb102e64
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-16 16:21:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |color, locations|
+    locations.each do |location, pigeons|
+      pigeons.each do |pigeon|
+        pigeon_list[pigeon] = {}
+        pigeon_list[pigeon][color] = []
+        pigeon_list[pigeon][color] << location.to_s
+      end
+    end
+  end
+  pigeon_list
 end
