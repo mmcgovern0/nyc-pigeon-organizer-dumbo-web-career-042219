7a5285d14a214674cff5399a2d7aa9110d6c8321
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-16 16:26:49 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |trait, options|
+    options.each do |option, pigeons|
+      pigeons.each do |pigeon|
+        pigeon_list[pigeon] ||= {}
+        pigeon_list[pigeon][trait] ||= []
+        pigeon_list[pigeon][trait] << option.to_s
+      end
+    end
+  end
+  pigeon_list
 end
